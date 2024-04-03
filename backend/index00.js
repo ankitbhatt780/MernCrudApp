a0c0fb4ac336b5eb7d997919b9bd58fc47fa04df
Ankit Bhatt <130929062+ankitbhatt780@users.noreply.github.com>
2024-04-03 22:26:52 +0530

    Update index00.js

--- backend/index00.js
+++ backend/index00.js
@@ -1,14 +1,14 @@
-const express = require("express")
-const cors = require("cors")
-const bodyParser = require("body-parser")
-const UserRouter = require("./Routes/UserRoute")
-const dbConnect = require("./DbConnect")
-PORT = 6060;
-dbConnect("mongodb://localhost:27017//mernStack")
-const app = express();
-app.use(cors)
-app.use(bodyParser.json)
+// const express = require("express")
+// const cors = require("cors")
+// const bodyParser = require("body-parser")
+// const UserRouter = require("./Routes/UserRoute")
+// const dbConnect = require("./DbConnect")
+// PORT = 6060;
+// dbConnect("mongodb://localhost:27017//mernStack")
+// const app = express();
+// app.use(cors)
+// app.use(bodyParser.json)
 
-app.use("api", UserRouter)
+// app.use("api", UserRouter)
 
-app.listen(PORT, () => console.log("server is running at " + PORT + "port"))
+// app.listen(PORT, () => console.log("server is running at " + PORT + "port"))
